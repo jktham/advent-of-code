4d60831f2803623e06fdaf19b3302a362a8eae80
jktham-pc <jonas.tham@gmx.ch>
2022-01-23 15:09:13 +0100

    Complete 4

--- 4.py
+++ 4.py
@@ -6,48 +6,50 @@ with open(r".\data\4.txt") as file:
 data = data.split("\n")
 data.append("")
 
-drawn = data[0].split(",")
+numbers = data[0].split(",")
+
+class Board:
+    def __init__(self, values, marked):
+        self.values = values
+        self.marked = marked
 
 boards = []
 for i in range(2, len(data)):
     if data[i] == "":
-        boards.append([row.split() for row in data[i-5:i]])
+        boards.append(Board([row.split() for row in data[i-5:i]], [[0]*5 for _ in range(5)]))
 
 def checkWin(i):
-    for j in range(len(boards[0])):
-        if sum(marked[i][j]) == 5:
+    for j in range(5):
+        if sum(boards[i].marked[j]) == 5:
             return True
-    for j in range(len(boards[0][0])):
-        if sum([row[j] for row in marked[i]]) == 5:
+    for j in range(5):
+        if sum([row[j] for row in boards[i].marked]) == 5:
             return True
 
 def calculateScore(i, n):
     score = 0
-    for j in range(len(boards[0])):
-        for k in range(len(boards[0][0])):
-            if marked[i][j][k] == 0:
-                score += int(boards[i][j][k])
+    for j in range(5):
+        for k in range(5):
+            if boards[i].marked[j][k] == 0:
+                score += int(boards[i].values[j][k])
     return score * int(n)
 
-marked = copy.deepcopy(boards)
-for i in range(len(boards)):
-    for j in range(len(boards[0])):
-        for k in range(len(boards[0][0])):
-            marked[i][j][k] = 0
+def drawNumbers(numbers):
+    scores = []
+    winners = []
+    for n in numbers:
+        for i in range(len(boards)):
+            for j in range(5):
+                for k in range(5):
+                    if int(boards[i].values[j][k]) == int(n):
+                        boards[i].marked[j][k] = 1 
+            if checkWin(i) and i not in winners:
+                score = calculateScore(i, n)
+                scores.append(score)
+                winners.append(i)
+    return scores
 
-stop = False
-scores = []
-for n in drawn:
-    if stop:
-        break
-    for i in range(len(boards)):
-        for j in range(len(boards[0])):
-            for k in range(len(boards[0][0])):
-                if int(boards[i][j][k]) == int(n):
-                    marked[i][j][k] = 1
-        if checkWin(i):
-            score = calculateScore(i, n)
-            scores.append(score)
+scores = drawNumbers(numbers)
 
 print(scores[0])
-print(scores)
+print(scores[-1])
